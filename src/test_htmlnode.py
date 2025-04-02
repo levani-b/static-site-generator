d13110fe3d097fe5504717704f395b88d1bb28bc
levani-b <levanbirchadze@gmail.com>
2025-04-02 14:27:30 +0400

    implement leafnode class for rendering html leaf elements add unit tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,8 +1,9 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
+    # tests for html node
     def test_to_html_props(self):
         node = HTMLNode('div', 'HEY YOU!', None, { "href": "https://www.google.com","target": "_blank",})
         self.assertEqual(node.props_to_html(), ' href="https://www.google.com" target="_blank"')
@@ -21,6 +22,30 @@ class TestHTMLNode(unittest.TestCase):
             "HTMLNode(div, HEY YOU, children: None, {'href': 'google.com'})"
         )
 
+    def test_when_props_none(self):
+        node = HTMLNode(
+            "div",
+            "Hello, world!",
+            None,
+            None
+        )
+        self.assertEqual(node.props_to_html(),
+            "",)
+
+
+    # tests for leaf node
+
+    def test_leaf_to_html_with_props(self):
+        node = LeafNode("p",'Hello',  {"src": "image.jpg", "alt": "An image"})
+        self.assertEqual(node.to_html(), '<p src="image.jpg" alt="An image">Hello</p>')
+
+    def test_leaf_to_html_no_props(self):
+        node = LeafNode("p",'Hello')
+        self.assertEqual(node.to_html(), '<p>Hello</p>')
+    
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, 'Hello')
+        self.assertEqual(node.to_html(), 'Hello')
 
 
 if __name__ == "__main__":
