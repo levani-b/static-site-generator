c981a03980805981a613fb7cede84842082beae5
levani-b <levanbirchadze@gmail.com>
2025-04-02 15:07:55 +0400

    add parentnode class for recursive html nesting with child elements

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -46,6 +46,53 @@ class TestHTMLNode(unittest.TestCase):
     def test_leaf_to_html_no_tag(self):
         node = LeafNode(None, 'Hello')
         self.assertEqual(node.to_html(), 'Hello')
+
+
+    # tests for parent node class
+    
+    def test_to_html_with_children(self):
+        child_node = LeafNode('span', 'child')
+        parent_node = ParentNode('p', [child_node])
+        self.assertEqual(parent_node.to_html(),"<p><span>child</span></p>")
+    
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("p", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<p><span><b>grandchild</b></span></p>",
+        )
+    
+    def test_to_html_many_children(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>",
+        )
+
+    def test_headings(self):
+        node = ParentNode(
+            "h2",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<h2><b>Bold text</b>Normal text<i>italic text</i>Normal text</h2>",
+        )
 
 
 if __name__ == "__main__":
